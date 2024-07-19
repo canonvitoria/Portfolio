280d97816dae67d9caad6828a89bbbe826fd457c
VitoriaCanon <vitoriacanon1212@gmail.com>
2024-07-19 18:51:49 -0300

    feat: :sparkles: Seção de Linguagens

--- app.js
+++ app.js
@@ -4,31 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const loadPage = async (page) => {
         try {
-            const response = await fetch(`Pages/${page}.html`);
+            const response = await fetch(`pages/${page}.html`);
             const content = await response.text();
             mainContent.innerHTML = content;
 
             if (page === 'projects') {
                 loadScript('scripts/script.js');
+            } else {
+                
             }
-
         } catch (error) {
             mainContent.innerHTML = '<p>Erro ao carregar a página.</p>';
         }
     };
 
-    const loadScript = (src) => {
+    const loadScript = (src, callback) => {
         const script = document.createElement('script');
         script.src = src;
         script.async = true;
+        script.onload = callback;
         document.body.appendChild(script);
-    };
-
-    const loadStyle = (href) => {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = href;
-        document.head.appendChild(link);
     };
 
     links.forEach(link => {
@@ -39,5 +34,34 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    loadPage('home'); // Carregar página inicial
+    const loadStyle = (href) => {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = href;
+        document.head.appendChild(link);
+    };
+
+    loadPage('home');
+});
+
+function animateCircle(circle, percent) {
+    const angle = (percent / 100) * 360;
+    const leftHalf = circle.querySelector('.circle-half-left');
+    const rightHalf = circle.querySelector('.circle-half-right');
+
+    if (angle <= 180) {
+        rightHalf.style.transform = `rotate(${angle}deg)`;
+        leftHalf.style.transform = `rotate(0deg)`;
+    } else {
+        rightHalf.style.transform = `rotate(180deg)`;
+        leftHalf.style.transform = `rotate(${angle - 180}deg)`;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const circles = document.querySelectorAll('.circle');
+    circles.forEach(circle => {
+        const percent = circle.getAttribute('data-percent');
+        animateCircle(circle, percent);
+    });
 });
--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,4 @@ document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
+  
