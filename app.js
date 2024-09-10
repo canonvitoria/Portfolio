671c2b5083c079af3b77ddeb750f27da2d752e90
VitoriaCanon <vitoriacanon1212@gmail.com>
2024-09-10 11:38:32 -0300

    feat: :sparkles: certification session

--- app.js
+++ app.js
@@ -4,70 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const loadPage = async (page) => {
         try {
-            const response = await fetch(`Pages/${page}.html`);
-            const content = await response.text();
-            mainContent.innerHTML = content;
-
-            if (page === 'projects') {
-                loadScript('scripts/script.js', () => {
-                    loadChartJs(() => {
-                        createCharts();
-                    });
-                });
-            }
-
-            if (page === 'formulario') {
-                loadStyle('styles/styleForms.css');
-            }
-        } catch (error) {
-            mainContent.innerHTML = '<p>Erro ao carregar a página.</p>';
-        }
-    };
-
-    const loadScript = (src, callback) => {
-        const script = document.createElement('script');
-        script.src = src;
-        script.async = true;
-        script.onload = callback;
-        document.body.appendChild(script);
-    };
-
-    const loadStyle = (href) => {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = href;
-        document.head.appendChild(link);
-    };
-
-    links.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            const page = link.getAttribute('data-page');
-            loadPage(page);
-        });
-    });
-
-    loadPage('home');
-});
-
-function loadChartJs(callback) {
-    const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/chart.js';
-    script.onload = callback;
-    document.body.appendChild(script);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('.navegacao a, .formularioRegistro');
-    const mainContent = document.getElementById('main-content');
-
-    const loadPage = async (page) => {
-        try {
             const response = await fetch(`pages/${page}.html`);
             const content = await response.text();
             mainContent.innerHTML = content;
 
             if (page === 'projects') {
+                loadStyle('styles/style.css');
                 loadScript('scripts/script.js', () => {
                     loadChartJs(() => {
                         createCharts();
@@ -78,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
             if (page === 'formulario') {
                 loadStyle('styles/styleForms.css');
             }
+
         } catch (error) {
             mainContent.innerHTML = '<p>Erro ao carregar a página.</p>';
         }
@@ -140,7 +83,7 @@ function createCharts() {
                 }
             },
             plugins: [{
-                beforeDraw: function(chart) {
+                beforeDraw: function (chart) {
                     const width = chart.width,
                         height = chart.height,
                         ctx = chart.ctx;
@@ -169,3 +112,4 @@ function createCharts() {
     createChart('typescriptChart', 70);
     createChart('reactChart', 20);
 }
+
--- scripts/script.js
+++ scripts/script.js
@@ -7,4 +7,13 @@ document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
-  
+
+  document.getElementById('nextCert').onclick = function(){
+    let lists = document.querySelectorAll('.item_certificate');
+    document.getElementById('list').appendChild(lists[0]);
+}
+
+document.getElementById('prevCert').onclick = function(){
+    let lists = document.querySelectorAll('.item_certificate');
+    document.getElementById('list').prepend(lists[lists.length - 1]);
+}
